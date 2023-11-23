b386d7de2233b6c2823890e0d6024281654e141d
metatarz <32838302+sirdmon@users.noreply.github.com>
2023-11-23 18:59:18 +0100

    adds schedule url

--- src/utils.ts
+++ src/utils.ts
@@ -1,62 +1,70 @@
 import { onDestroy } from "svelte";
 import { config } from "./config";
 
+export const REFRESH_RATE_MS: number = config.refreshRate;
+export const CS_ENDPOINT: string = `${config.carbstatusApiUrl}/index`;
 
-  export const REFRESH_RATE_MS: number =config.refreshRate
-  export const CS_ENDPOINT: string = `${config.carbstatusApiUrl}/index`
+export function onInterval(callback: any) {
+  const interval = setInterval(callback, REFRESH_RATE_MS);
 
-
-export function onInterval(callback:any) {
-	const interval = setInterval(callback, REFRESH_RATE_MS);
-
-	onDestroy(() => {
-		clearInterval(interval);
-	});
+  onDestroy(() => {
+    clearInterval(interval);
+  });
 }
 
-export async function getData(domain): Promise<IIndexData>{
+export async function getData(domain): Promise<IIndexData> {
+  if (config.isProd) {
+    const loc = window.location.hostname;
 
-    if(config.isProd){
-        const loc = window.location.hostname
+    if (/^localhost$|^127(\.[0-9]+){0,2}\.[0-9]+$|^\[::1?\]$/.test(loc) || window.location.protocol === 'file:') {
+      throw new Error('Ignore localhost')
+    }
+    //@ts-ignore
+    if (window._phantom || window.__nightmare || window.navigator.webdriver || window.Cypress) {
+      throw new Error('Ignore bots')
+    }
+    const domainRegexp = new RegExp(domain)
 
-        if(loc !== domain){
-            throw new Error(`Domain mismatch: ${loc}`)
-        }
-
-        const res = await fetch(`${CS_ENDPOINT}?l=https://${domain}`)
-
-        if (!(res && res.ok)){
-            if(res.status === 400){
-                const msg = await res.json()
-                throw new Error(`BadRequest: ${msg}`)
-            }
-            throw new Error(`Unknown error`)
-        }
-        const msg:IIndexData = await res.json()
-
-        return msg
+    if(!domainRegexp.test(loc)){
+      console.warn(`[CarbStatus]: Domain mismatch: ${loc}`);
     }
 
-    return new Promise((resolve)=>setTimeout(()=>resolve(mockData()), 3000))
+    const res = await fetch(`${CS_ENDPOINT}?l=https://${domain}`);
+
+    if (!(res && res.ok)) {
+      if (res.status === 400) {
+        const msg = await res.json();
+        throw new Error(`BadRequest: ${msg}`);
+      }
+      if(res.status === 404 && !domain.startsWith('www')){
+        return getData('www.'+domain)
+      }
+      throw new Error(`Unknown error`);
+    }
+    const msg: IIndexData = await res.json();
+
+    return msg;
+  }
+
+  return new Promise((resolve) => setTimeout(() => resolve(mockData()), 3000));
 }
 
-
-
-export function info(msg:string){
-    console.log(`[CarbStatus]: ${msg}`)
+export function info(msg: string) {
+  console.log(`[CarbStatus]: ${msg}`);
 }
 
-export function panic(msg:string){
-    console.error(`[CarbStatus]: ${msg}`)
+export function panic(msg: string) {
+  console.error(`[CarbStatus]: ${msg}`);
 }
 
-
-export interface IIndexData{
-    time: number,
-    nvalue: number
-    value: number
+export interface IIndexData {
+  time: number;
+  nvalue: number;
+  value: number;
 }
 
-const mockData =():IIndexData=>({"time":(Date.now() + 4 * 1000 * 60) / 1000,"nvalue":Math.floor(Math.random()*100),"value":440.19831885344405})
-
-
+const mockData = (): IIndexData => ({
+  time: (Date.now() + 4 * 1000 * 60) / 1000,
+  nvalue: Math.floor(Math.random() * 100),
+  value: 440.19831885344405,
+});
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
     readonly VITE_CARBSTATUS_API_URL: string;
     readonly VITE_CARBSTATUS_URL: string
     readonly VITE_PROD: boolean
+    readonly VITE_CARBSTATUS_SCHEDULE_URL: string
     
 }
   
